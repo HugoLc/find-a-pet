17443bfdb27be9860a20e4c3f0f15a79146f16a4
HugoLc <hugolindoso.c@gmail.com>
2022-07-26 21:25:44 -0300

    unificado os dados do form

--- client/src/components/QuizForm/index.jsx
+++ client/src/components/QuizForm/index.jsx
@@ -5,28 +5,50 @@ import SelectBar from './SelectBar';
 import SelectBoxes from './SelectBoxes';
 
 const QuizForm = () => {
-  const [especie, setEspecie] = useState();
-  const [sexo, setSexo] = useState();
-  const [idade, setIdade] = useState();
-  const [porte, setPorte] = useState();
-  const [selectedPetList, setSelectedPetList] = useState([]);
-
-
-
-
+  const [especie, setEspecie] = useState("todos");
+  const [sexo, setSexo] = useState("todos");
+  const [idade, setIdade] = useState("todos");
+  const [porte, setPorte] = useState("todos");
   const [uf, setUf] = React.useState('AC');
   const [listUf, setListUf] = React.useState([]);
-  const [city, setCity] = React.useState('');
+  const [city, setCity] = React.useState('Acrelândia');
   const [listCity, setListCity] = React.useState([]);
   const [valueAtividade, setValueAtividade] = useState(0)
   const [valueDisponibilidade, setValueDisponibilidade] = useState(0)
+  const [residencia, setResidencia] = useState(0)
+  const [selectedPetList, setSelectedPetList] = useState([]);
 
+  const [formObject, setFormObject] = useState()
 
   useEffect(() => {
-    // console.log("valueAtividade",valueAtividade)
-    // console.log("valueDisponibilidade",valueDisponibilidade)
+  }, [selectedPetList])
   
-  }, [valueAtividade,valueDisponibilidade])
+
+  const handleSubmit = () => {
+    console.log("entrei")
+    const formData = {
+      especie: especie.toUpperCase(),
+      sexo: sexo.toUpperCase(),
+      idade: idade.toUpperCase(),
+      porte: porte.toUpperCase(),
+      estado: uf.toUpperCase(),
+      cidade: city.toUpperCase(),
+      atividade: valueAtividade,
+      disponibilidade: valueDisponibilidade,
+      residencia: residencia.toUpperCase(),
+      criancas: selectedPetList.includes("Crianças pequenas"),
+      petCaes: selectedPetList.includes("Cão"),
+      petGatos: selectedPetList.includes("Gato"),
+      petOutros: selectedPetList.includes("Outros")
+    }
+
+    setFormObject(formData)
+  }
+
+  useEffect(() => {
+    /////////////////////////////////////// post api
+    formObject && console.log(formObject)
+  }, [formObject])
   
 
   function loadUf() {
@@ -49,87 +71,87 @@ const QuizForm = () => {
           setListCity([...data]);
          });
   }
-  React.useEffect(() => {
+  useEffect(() => {
     loadUf();
   },[]);
-  React.useEffect(() => {
+  useEffect(() => {
     if (uf) {
       loadCity(uf);
     }
   }, [uf]);
 
-  const selectPetCrianca = (nome) => {
-    console.log("nome",nome)
-    /* if ( selectedIndex === -1) {
-      setSelectedPetList([...selectedPetList, nome])
-    } else {
-      setSelectedPetList(selectedPetList.splice(selectedIndex, 1))
-    } */
-  }
+  /* useEffect(() => {
+  },[selectedPetList]) */
+
+  /* const selectPetCrianca = (nomeList) => {
+    console.log("nome",nomeList)
+    setSelectedPetList(nomeList)
+    console.log("selectedPetList",selectedPetList);
+  } */
 
   return (
     <div className={styles.formWrapper}>
-        <label> Qual seu estado?
-          <select value={uf} onChange={e => setUf(e.target.value)}>
-            {listUf.map((a, b) => ( 
-                <option key={b}value={a.id}>{a.sigla} - {a.nome}</option>
-            ))}
-          </select>
-        </label>
-        <label> Qual sua cidade?
-          <select value={city} onChange={e => setCity(e.target.value)}>
-            {listCity.map((a, b) => ( 
-                <option key={b} value={a.sigla}>{a.nome}</option>
-            ))}
-          </select>
-        </label>
-        <label> Qual espécie procura?
-          <select value={especie} defaultValue={"TODOS"}  onChange={(e) => setEspecie(e.target.value)}>
-            <option value="TODOS" selected>Todos</option>
-            <option value={"GATO"}>Gato</option>
-            <option value={"CACHORRO"}>Cachorro</option>
-          </select>
-        </label>
-        <label> Qual o sexo?
-          <select value={sexo} defaultValue={"TODOS"} onChange={(e) => setSexo(e.target.value)}>
-            <option value="TODOS">Todos</option>
-            <option value={"M"}>Macho</option>
-            <option value={"F"}>Fêmea</option>
-          </select>
-        </label>
-        <label> Qual idade?
-          <select value={idade} defaultValue={"TODOS"} onChange={(e) => setIdade(e.target.value)}>
-            <option value="TODOS" selected>Todos</option>
-            <option value={"FILHOTE"}>Quero um filhote</option>
-            <option value={"ADULTO"}>Quero um(a) companheiro(a) adulto(a)</option>
-            <option value={"IDOSO"}>Quero tornar um  bichinho idoso feliz</option>
-          </select>
-        </label>
-        <label> Qual o tamanho?
-          <select value={porte} defaultValue={"TODOS"} onChange={(e) => setPorte(e.target.value)}>
-            <option value="TODOS" selected>Todos</option>
-            <option value={"P"}>Pequeno</option>
-            <option value={"M"}>Médio</option>
-            <option value={"G"}>Grande</option>
-          </select>
-        </label>
-        <label > Como diria que é seu nível de atividade?
-            <SelectBar min="Calmo" max="Agitado" onSelect={(value)=> setValueAtividade(value)}/>
-        </label>
-        <label > Quanto tempo em média terá disponível para o pet?
-            <SelectBar min="Pouco" max="Muito" onSelect={value => setValueDisponibilidade(value)}/>
-        </label>
-        <label> Casa ou apartamento?
-          <select value={porte} defaultValue={"TODOS"} onChange={(e) => setPorte(e.target.value)}>
-            <option value="TODOS" selected>Todos</option>
-            <option value={"CASA"}>Casa</option>
-            <option value={"APARTAMENTO"}>Apartamento</option>
-          </select>
-        </label>
-        <label> Há outro crianças e/ou pet na casa?
-          <SelectBoxes nomesBoxes={["Cão","Gato","Crianças pequenas","Outros"]} onSelect={selectPetCrianca}/>
-        </label>
-
+      <label> Qual seu estado?
+        <select value={uf} onChange={e => setUf(e.target.value)}>
+          {listUf.map((a, b) => ( 
+              <option key={b}value={a.id}>{a.sigla} - {a.nome}</option>
+          ))}
+        </select>
+      </label>
+      <label> Qual sua cidade?
+        <select value={city} onChange={e => setCity(e.target.value)}>
+          {listCity.map((a, b) => ( 
+              <option key={b} value={a.sigla}>{a.nome}</option>
+          ))}
+        </select>
+      </label>
+      <label> Qual espécie procura?
+        <select value={especie} defaultValue={"TODOS"}  onChange={(e) => setEspecie(e.target.value)}>
+          <option value="TODOS" selected>Todos</option>
+          <option value={"GATO"}>Gato</option>
+          <option value={"CACHORRO"}>Cachorro</option>
+        </select>
+      </label>
+      <label> Qual o sexo?
+        <select value={sexo} defaultValue={"TODOS"} onChange={(e) => setSexo(e.target.value)}>
+          <option value="TODOS">Todos</option>
+          <option value={"M"}>Macho</option>
+          <option value={"F"}>Fêmea</option>
+        </select>
+      </label>
+      <label> Qual idade?
+        <select value={idade} defaultValue={"TODOS"} onChange={(e) => setIdade(e.target.value)}>
+          <option value="TODOS" selected>Todos</option>
+          <option value={"FILHOTE"}>Quero um filhote</option>
+          <option value={"ADULTO"}>Quero um(a) companheiro(a) adulto(a)</option>
+          <option value={"IDOSO"}>Quero tornar um  bichinho idoso feliz</option>
+        </select>
+      </label>
+      <label> Qual o tamanho?
+        <select value={porte} defaultValue={"TODOS"} onChange={(e) => setPorte(e.target.value)}>
+          <option value="TODOS" selected>Todos</option>
+          <option value={"P"}>Pequeno</option>
+          <option value={"M"}>Médio</option>
+          <option value={"G"}>Grande</option>
+        </select>
+      </label>
+      <label > Como diria que é seu nível de atividade?
+          <SelectBar min="Calmo" max="Agitado" onSelect={(value)=> setValueAtividade(value)}/>
+      </label>
+      <label > Quanto tempo em média terá disponível para o pet?
+          <SelectBar min="Pouco" max="Muito" onSelect={value => setValueDisponibilidade(value)}/>
+      </label>
+      <label> Casa ou apartamento?
+        <select value={residencia} defaultValue={"TODOS"} onChange={(e) => setResidencia(e.target.value)}>
+          <option value="TODOS" selected>Todos</option>
+          <option value={"CASA"}>Casa</option>
+          <option value={"APARTAMENTO"}>Apartamento</option>
+        </select>
+      </label>
+      <label> Há outro pet e/ou crianças na casa?
+        <SelectBoxes nomesBoxes={["Cão","Gato","Crianças pequenas","Outros"]} onSelect={nomeList => setSelectedPetList(nomeList)}/>
+      </label>
+      <button className={styles.enviarBtn} onClick={()=> handleSubmit()}> Enviar </button>
 
     </div>
   )
